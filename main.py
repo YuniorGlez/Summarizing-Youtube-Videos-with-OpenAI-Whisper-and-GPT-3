2f188571bfba1dda7614e2bae97f6bd57d0bb1a1
YuniorGlez <yunior@squaads.com>
2024-06-01 21:03:39 +0100

    🚀 feat: Create main.py to handle program execution

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+from src.summarize_youtube_videos import boot
+
+load_dotenv()  # take environment variables from .env.
+
+if __name__ == '__main__':
+    while True:
+        boot()
+        continue_prompt = input("Do you want to summarize another video? (y/n): ")
+        if continue_prompt.lower() != 'y':
+            break
